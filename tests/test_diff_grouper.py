81d14be459cb9e0a75f1f592d745c4c8bdea411f
Pradeep Tammali <pradeepkumartammali@gmail.com>
2024-06-22 22:29:32 +0200

    fix: merge the missing groups with max annotation gap

--- tests/test_diff_grouper.py
+++ tests/test_diff_grouper.py
@@ -65,7 +65,8 @@ def test_group_branch_annotations(coverage_obj, diff_coverage_obj):
     result = diff_grouper.get_branch_missing_groups(coverage=coverage_obj, diff_coverage=diff_coverage_obj)
 
     assert list(result) == [
-        groups.Group(file=pathlib.Path('codebase/code.py'), line_start=5, line_end=11),
+        groups.Group(file=pathlib.Path('codebase/code.py'), line_start=5, line_end=6),
+        groups.Group(file=pathlib.Path('codebase/code.py'), line_start=10, line_end=11),
     ]
 
 
@@ -76,6 +77,9 @@ def test_group_branch_annotations_more_files(coverage_obj_more_files, diff_cover
     )
 
     assert list(result) == [
-        groups.Group(file=pathlib.Path('codebase/code.py'), line_start=4, line_end=8),
-        groups.Group(file=pathlib.Path('codebase/other.py'), line_start=2, line_end=9),
+        groups.Group(file=pathlib.Path('codebase/code.py'), line_start=4, line_end=5),
+        groups.Group(file=pathlib.Path('codebase/code.py'), line_start=7, line_end=8),
+        groups.Group(file=pathlib.Path('codebase/other.py'), line_start=2, line_end=3),
+        groups.Group(file=pathlib.Path('codebase/other.py'), line_start=4, line_end=5),
+        groups.Group(file=pathlib.Path('codebase/other.py'), line_start=8, line_end=9),
     ]
